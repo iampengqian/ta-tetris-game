27a639839164bb5da2c35af0f955b1d8f1acc7f5
Peng Qian <pengqian@pengqian.net>
2026-03-28 17:56:31 +0800

    refactor: fix implicit globals, update event properties, improve transitions, and add tests

--- script.js
+++ script.js
@@ -33,17 +33,17 @@ function drawSquare(x, y, color, context = ctx) {
 // Create board
 let board = [];
 function initBoard() {
-    for (r = 0; r < ROW; r++) {
+    for (let r = 0; r < ROW; r++) {
         board[r] = [];
-        for (c = 0; c < COL; c++) {
+        for (let c = 0; c < COL; c++) {
             board[r][c] = VACANT;
         }
     }
 }
 
 function drawBoard() {
-    for (r = 0; r < ROW; r++) {
-        for (c = 0; c < COL; c++) {
+    for (let r = 0; r < ROW; r++) {
+        for (let c = 0; c < COL; c++) {
             drawSquare(c, r, board[r][c]);
         }
     }
@@ -61,8 +61,8 @@ function Piece(tetromino, color) {
 }
 
 Piece.prototype.fill = function(color, context = ctx, offsetX = 0, offsetY = 0) {
-    for (r = 0; r < this.activeTetromino.length; r++) {
-        for (c = 0; c < this.activeTetromino.length; c++) {
+    for (let r = 0; r < this.activeTetromino.length; r++) {
+        for (let c = 0; c < this.activeTetromino.length; c++) {
             if (this.activeTetromino[r][c]) {
                 drawSquare(this.x + c + offsetX, this.y + r + offsetY, color, context);
             }
@@ -131,8 +131,8 @@ Piece.prototype.rotate = function() {
 };
 
 Piece.prototype.collision = function(x, y, piece) {
-    for (r = 0; r < piece.length; r++) {
-        for (c = 0; c < piece.length; c++) {
+    for (let r = 0; r < piece.length; r++) {
+        for (let c = 0; c < piece.length; c++) {
             if (!piece[r][c]) continue;
             
             let newX = this.x + c + x;
@@ -147,8 +147,8 @@ Piece.prototype.collision = function(x, y, piece) {
 };
 
 Piece.prototype.lock = function() {
-    for (r = 0; r < this.activeTetromino.length; r++) {
-        for (c = 0; c < this.activeTetromino.length; c++) {
+    for (let r = 0; r < this.activeTetromino.length; r++) {
+        for (let c = 0; c < this.activeTetromino.length; c++) {
             if (!this.activeTetromino[r][c]) continue;
             
             if (this.y + r < 0) {
@@ -163,22 +163,21 @@ Piece.prototype.lock = function() {
     }
     
     let linesClearedThisTurn = 0;
-    for (r = 0; r < ROW; r++) {
+    for (let r = 0; r < ROW; r++) {
         let isFull = true;
-        for (c = 0; c < COL; c++) {
+        for (let c = 0; c < COL; c++) {
             if (board[r][c] === VACANT) {
                 isFull = false;
                 break;
             }
         }
         if (isFull) {
-            // Fix: Changed y > 1 to y > 0 to include the first row
-            for (y = r; y > 0; y--) {
-                for (c = 0; c < COL; c++) {
+            for (let y = r; y > 0; y--) {
+                for (let c = 0; c < COL; c++) {
                     board[y][c] = board[y - 1][c];
                 }
             }
-            for (c = 0; c < COL; c++) {
+            for (let c = 0; c < COL; c++) {
                 board[0][c] = VACANT;
             }
             linesClearedThisTurn++;
@@ -214,10 +213,10 @@ function updateScore(cleared) {
 }
 
 function randomPiece() {
-    let r = Math.floor(Math.random() * PIECES.length);
+    let rIndex = Math.floor(Math.random() * PIECES.length);
     // Deep clone the tetromino pattern to prevent template pollution
-    const tetrominoCopy = JSON.parse(JSON.stringify(PIECES[r][0]));
-    return new Piece(tetrominoCopy, PIECES[r][1]);
+    const tetrominoCopy = JSON.parse(JSON.stringify(PIECES[rIndex][0]));
+    return new Piece(tetrominoCopy, PIECES[rIndex][1]);
 }
 
 function drawNextPiece() {
@@ -245,23 +244,23 @@ function drawNextPiece() {
 document.addEventListener("keydown", event => {
     if (gameOver) return;
     
-    if (event.keyCode === 80) { // 'P' key
+    if (event.key === 'p' || event.key === 'P') {
         togglePause();
         return;
     }
     
     if (isPaused) return;
 
-    if (event.keyCode === 37) {
+    if (event.key === 'ArrowLeft') {
         currentPiece.moveLeft();
         dropStart = Date.now();
-    } else if (event.keyCode === 38) {
+    } else if (event.key === 'ArrowUp') {
         currentPiece.rotate();
         dropStart = Date.now();
-    } else if (event.keyCode === 39) {
+    } else if (event.key === 'ArrowRight') {
         currentPiece.moveRight();
         dropStart = Date.now();
-    } else if (event.keyCode === 40) {
+    } else if (event.key === 'ArrowDown') {
         currentPiece.moveDown();
     }
 });
